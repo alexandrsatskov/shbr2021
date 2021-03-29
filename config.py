60d76d12eac91c47a7afaae1cc8285d604c2ae5b
twinkleToes2001 <s.satskov@gmail.com>
2021-03-29 15:15:25 +0500

    '.'

--- config.py
+++ config.py
@@ -3,13 +3,12 @@
 DBMS = 'postgresql'
 DATABASE_HOST = '127.0.0.1'
 DATABASE_PORT = '5432'
+DATABASE_NAME = 'postgres'
 
 # For local use
-DATABASE_USERNAME = 'postgres'
+# DATABASE_USERNAME = 'postgres'
+# DATABASE_PASSWORD = 'root'
+
+# Dev platform
+DATABASE_USERNAME = 'dev'
 DATABASE_PASSWORD = 'root'
-
-# # # Dev platform
-# DATABASE_USERNAME = 'python_dev6'
-# DATABASE_PASSWORD = 'BfoqTxDARl'
-
-DATABASE_NAME = DATABASE_USERNAME
